a0579212b7041a3f17aa303936c96596a356ff61
Lawal Akande <lawalsaliouakande@gmail.com>
2020-07-08 06:53:44 -0700

    My w3shool

---
+++ myJavaScript.js
@@ -0,0 +1,5 @@
+function myFunction() {
+     var myProgramFOrTheDay = ['Salat', 'Work', 'eat', 'sleep', 'Game'];
+     myProgramFOrTheDay.sort();
+     document.getElementById('demo').innerHTML = myProgramFOrTheDay;
+}
